5f53fc5e170603f777f039e6f4c766ba1f60f14b
vigneshpriya <51686518+vigneshpriya@users.noreply.github.com>
2019-06-12 14:43:24 +0530

    Update big.py

--- code kata/big.py
+++ code kata/big.py
@@ -1,9 +1,8 @@
-bi1=int(input())
-bi2=int(input())
-bi3=int(input())
-if(bi1>=bi2 and bi1>=bi3 ):
-  print(bi1)
-elif(bi2>=bi1 and bi2>=bi3):
-  print(bi2)
-elif(bi3>=bi1 and bi3>=bi2):
-  print(bi3)
+a1,b2,c3=map(int,input().split())
+if a1>b2:
+  if a1>c3:
+    print(a1)
+elif b2>c3:
+  print(b2)
+else:
+  print(c3)
