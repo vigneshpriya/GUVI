a3389c3ac14c1db13b5bf1ee7cf5fe8a60ceccf4
vigneshpriya <51686518+vigneshpriya@users.noreply.github.com>
2019-06-12 11:50:29 +0530

    Create vowel.py

---
+++ code kata/vowel.py
@@ -0,0 +1,7 @@
+vo=input()
+if(vo=='A' or vo=='E' or vo=='I' or vo=='O' or vo=='U' or vo=='a' or vo=='e' or vo=='i' or vo=='o' or vo=='u'):
+  print("Vowel") 
+elif(vo=='!' or vo=='@' or vo=='#' or vo=='$' or vo=='&'):
+  print("invalid")
+else:
+  print("Consonant")
