1ff74d9a696658a54fbcec2a6ff892a976abb619
vigneshpriya <51686518+vigneshpriya@users.noreply.github.com>
2019-06-12 12:00:54 +0530

    Create alphabet.py

---
+++ code kata/alphabet.py
@@ -0,0 +1,5 @@
+al=input()
+if(al=='a' or al=='b' or al=='c' or al=='d' or al=='e' or al=='f' or al=='g' or al=='h' or al=='i' or al=='j' or al=='k' or al=='l' or  al=='m' or al=='n' or al=='o' or al=='p' or al=='q' or al=='r' or al=='s' or al=='t' or al=='u' or al=='v' or al=='w' or al=='x' or al=='y' or al=='z' or al=='A' or al=='B' or al=='C' or al=='D' or al=='E' or al=='F' or al=='G' or al=='H' or al=='I' or al=='J' or al=='K' or al=='L' or  al=='M' or al=='N' or al=='O' or al=='P' or al=='Q' or al=='R' or al=='S' or al=='T' or al=='U' or al=='V' or al=='W' or al=='X' or al=='Y' or al=='Z'):
+  print("Alphabet")
+else:
+  print("No")
