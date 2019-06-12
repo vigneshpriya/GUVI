5d18473bdee1ab042c308f7f88fa68bc75d5ff20
vigneshpriya <51686518+vigneshpriya@users.noreply.github.com>
2019-06-12 12:11:24 +0530

    Create big.py

---
+++ code kata/big.py
@@ -0,0 +1,9 @@
+bi1=int(input())
+bi2=int(input())
+bi3=int(input())
+if(bi1>=bi2 and bi1>=bi3 ):
+  print(bi1)
+elif(bi2>=bi1 and bi2>=bi3):
+  print(bi2)
+elif(bi3>=bi1 and bi3>=bi2):
+  print(bi3)
