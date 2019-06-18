25a1b2925d33e65e26a03fed779b54ac26c5f3b0
vigneshpriya <51686518+vigneshpriya@users.noreply.github.com>
2019-06-18 18:24:03 +0530

    Create roman to number.py

---
+++ code kata/roman to number.py
@@ -0,0 +1,5 @@
+rom =(input())
+num=["Zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for j in range(21):
+  if rom==num[j]:
+    print(j)
